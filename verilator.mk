987a8695277d684b1c87d209183b12f803df37a6
LinMingfeng <lin_0f@qq.com>
2022-11-16 14:12:27 +0800

    modify clang --> g++

--- verilator.mk
+++ verilator.mk
@@ -88,7 +88,7 @@ endif
 # --trace
 VERILATOR_FLAGS =                   \
   --top-module $(EMU_TOP)           \
-  --compiler clang                  \
+  --compiler g++                    \
   +define+VERILATOR=1               \
   +define+DIFFTEST                  \
   +define+PRINTF_COND=1             \
@@ -126,7 +126,7 @@ EMU_COMPILE_FILTER =
 build_emu_local: $(EMU_MK)
 	@echo "\n[g++] Compiling C++ files..." >> $(TIMELOG)
 	@date -R | tee -a $(TIMELOG)
-	$(TIME_CMD) $(MAKE) CXX=clang++ LINK=clang++  VM_PARALLEL_BUILDS=1 OPT_FAST="-O3" -C $(<D) -f $(<F) $(EMU_COMPILE_FILTER)
+	$(TIME_CMD) $(MAKE) CXX=g++ LINK=g++  VM_PARALLEL_BUILDS=1 OPT_FAST="-O3" -C $(<D) -f $(<F) $(EMU_COMPILE_FILTER)
 
 $(EMU): $(EMU_MK) $(EMU_DEPS) $(EMU_HEADERS) $(REF_SO)
 ifeq ($(REMOTE),localhost)
